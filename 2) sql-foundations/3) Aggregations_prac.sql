be959747b6f8f98f9de59c92c633c52103fff933
SREEKAR-projects <sreekarrv21@gmail.com>
2026-04-10 17:51:44 +0530

    Update

--- 2) sql-foundations/3) Aggregations_prac.sql
+++ 2) sql-foundations/3) Aggregations_prac.sql
@@ -7,3 +7,44 @@ from claims_flat;
 select sum(amount) as total_billed
 from claims_flat;
 
+select ROUND(AVG(amount), 2)
+from claims_flat;
+
+select max(amount)
+from claims_flat;
+
+SELECT member_name
+FROM claims_flat 
+ORDER BY amount DESC 
+LIMIT 1;
+
+SELECT count(`status`)
+FROM claims_flat 
+where `status` = "approved";
+
+SELECT count(`status`)
+FROM claims_flat 
+where `status` = "rejected";
+
+select avg(amount)
+FROM claims_flat 
+where datediff(claim_date,policy_start) < 30;
+
+select avg(amount)
+FROM claims_flat 
+where datediff(claim_date,policy_start) > 30; #Inception fraud is taking more amount than others
+
+select sum(amount) as total_claims_paid
+from claims_flat
+where `status` = "approved";
+
+select sum(amount) as total_billed_grt_thn_100k
+from claims_flat
+where amount >100000;
+
+select sum(amount) as total_billed
+from claims_flat;
+
+#total_billed_grt_thn_100k/total_billed*100
+
+
