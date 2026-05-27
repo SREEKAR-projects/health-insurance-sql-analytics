6b746ec34f4deb2b621f23bb37f6980ed929e9d3
SREEKAR-projects <sreekarrv21@gmail.com>
2026-05-28 00:16:37 +0530

    Update

--- 2) sql-foundations/0) Revision.sql
+++ 2) sql-foundations/0) Revision.sql
@@ -4,14 +4,20 @@ select*
 from providers;
 
 select*
-from claims;
+from claims_flat;
 
-SELECT provider_id,
-       AVG(claim_amount)
-FROM claims
-GROUP BY provider_id
-HAVING AVG(claim_amount) >
-       1.4 * (
-           SELECT AVG(claim_amount)
-           FROM claims
-       );
+SELECT claim_id
+from claims_flat
+where member_id = 'M101' and city = "Hyderabad" and provider like "%Apollo%" and amount = 420000;
+
+select avg(amount)
+from claims_flat
+where datediff(claim_date,policy_start) < 30
+union
+select avg(amount)
+from claims_flat
+where datediff(claim_date,policy_start) > 30;  #in this table claim_amount is amount and less than 30 is potential inception fraud 
+
+select provider,avg(amount)
+from claims_flat
+where avg(amount) >= 1.4*(select avg(amount) from claims_flat)
